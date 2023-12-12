c10010dd8d58aae4da5fcbfdbc4e97823f20ece5
seunghyun <tmdgus7820@naver.com>
2023-12-13 01:57:54 +0900

    [design] : 반응형 css 수정

--- src/components/search/Search.vue
+++ src/components/search/Search.vue
@@ -1,20 +1,4 @@
 <template>
-  <button
-    type="button"
-    class="search-btn"
-    @click="searchToggle">
-    <span>검색</span>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      enable-background="new 0 0 24 24"
-      height="24"
-      viewBox="0 0 24 24"
-      width="24"
-      focusable="false">
-      <path
-        d="m20.87 20.17-5.59-5.59C16.35 13.35 17 11.75 17 10c0-3.87-3.13-7-7-7s-7 3.13-7 7 3.13 7 7 7c1.75 0 3.35-.65 4.58-1.71l5.59 5.59.7-.71zM10 16c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6z"></path>
-    </svg>
-  </button>
   <form
     ref="searchForm"
     @submit.prevent="searchMovies">
@@ -62,12 +46,6 @@ function searchMovies() {
     title.value = '';
   }
 }
-
-function searchToggle() {
-  if (searchForm.value) {
-    (searchForm.value as HTMLElement).classList.toggle('on');
-  }
-}
 </script>
 
 <style scoped lang="scss">
@@ -109,31 +87,6 @@ function searchToggle() {
       svg {
         fill: #fff;
       }
-    }
-  }
-  .search-btn {
-    display: none;
-    justify-content: space-between;
-    align-items: center;
-    border: 0;
-    outline: 0;
-    background-color: #e13232;
-    border-radius: 5px;
-    color: #fff;
-    padding: 5px;
-    cursor: pointer;
-    transition: all 0.2s;
-
-    &:hover {
-      background-color: #af3535;
-    }
-
-    span {
-      margin-right: 5px;
-    }
-
-    svg {
-      fill: #fff;
     }
   }
 }
